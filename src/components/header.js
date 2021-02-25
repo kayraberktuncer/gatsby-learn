070eb06265917ca3abd7aa66572c8c56128ab85c
Kayra <kayrabtuncer@gmail.com>
2021-02-25 17:15:26 +0300

    Know Gatsby Building Blocks

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default function Header(props) {
+    return (
+        <div>
+            <h1>{props.headerText}</h1>
+            <Link to="/">Home</Link>
+            <Link to="/about/">About Page</Link>
+            <Link to="/contact/">Contact</Link>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Header headerText="Home Page" />
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  );
 }
